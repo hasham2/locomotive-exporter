9c75fc0ee5ca5504bb13f09a0c2c2e514e65c713
TFG <team@thefrontiergroup.com.au>
2011-05-16 12:51:55 +0800

    The database is now cleared after each test file runs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,11 @@ require 'rspec/rails'
 # Require support files
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-Spec::Runner.configure do |config|
+Rspec.configure do |config|
 
   config.mock_with :rr
 
-  config.before :all do
+  config.before :each do
     Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
   end
 
